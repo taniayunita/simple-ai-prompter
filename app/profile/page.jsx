de618b1a06c1b85ef7c7aca5063e51e4d0a64398
Tania Yunita <tania.yunita@wgs-id.com>
2023-08-31 00:08:55 +0700

    add profile id page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -19,13 +19,13 @@ const MyProfile = () => {
 
             setMyPosts(data);
         };
+
         if (session?.user.id) fetchPosts();
     }, [session?.user.id]);
 
     const handleEdit = (post) => {
         router.push(`/update-prompt?id=${post._id}`);
     };
-
     const handleDelete = async (post) => {
         const hasConfirmed = confirm(
             "Are you sure you want to delete this prompt?"
@@ -45,10 +45,9 @@ const MyProfile = () => {
             }
         }
     };
-
     return (
         <Profile
-            name='Tania Yunita'
+            name='My'
             desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
             data={myPosts}
             handleEdit={handleEdit}

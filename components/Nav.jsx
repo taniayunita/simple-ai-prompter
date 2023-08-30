9e6af87ab3140b4c70098a611420c0e82cd0fc13
Tania Yunita <tania.yunita@wgs-id.com>
2023-08-31 00:03:24 +0700

    finish CRUD prompt

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -38,7 +38,7 @@ const Nav = () => {
             <Link className="black_btn" href="/create-prompt">Create Post</Link>
             <button type="button" onClick={signOut} className="outline_btn"> Sign Out</button>
             <Link href="/profile">
-              <Image src={ImageProfile} width={37} height={37} className="rounded-full" alt="profile" />
+              <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="profile" />
             </Link>
           </div>
         ) : (
@@ -54,7 +54,7 @@ const Nav = () => {
       <div className="sm:hidden flex relative">
         {session?.user ? (
           <div className="flex">
-            <Image src={ImageProfile} width={37} height={37} className="rounded-full" alt="profile" onClick={() => setToggleDropDown((prev) => !prev)} />
+            <Image src={session?.user.image} width={37} height={37} className="rounded-full" alt="profile" onClick={() => setToggleDropDown((prev) => !prev)} />
             {toggleDropDown && (
               <div className="dropdown">
                 <Link href="/profile" className="dropdown_link" onClick={() => setToggleDropDown(false)}>
